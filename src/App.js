7eac73b74bbd17ed32d838d813f83eeb82c31360
danghanh99 <ldhanh1999@gmail.com>
2021-03-24 14:51:29 +0700

    RJT-3: revert app.js because confused with PR RJT 15

--- src/App.js
+++ src/App.js
@@ -1,31 +1,114 @@
-import { useState } from "react";
 import "./App.css";
-import TodoList from "./components/TodoList";
 
 function App() {
-  const [todoList, setTodoList] = useState([
-    { id: 1, title: "hello guys" },
-    { id: 2, title: "novahub " },
-    { id: 3, title: "owt" },
-  ]);
-
-  function handleTodoClick(todo) {
-    console.log(todo);
-    const index = todoList.findIndex((x) => x.id === todo.id);
-    if (index < 0) return;
-
-    const newTodoList = [... todoList];
-    newTodoList.splice(index, 1);
-    setTodoList(newTodoList);
-  }
-  console.log(todoList.length)
   return (
-    <div>
-      <div className="app">
-        <TodoList todos={todoList} onTodoClick={handleTodoClick} />
-
+    <div className="App">
+      <div className="page-content page-container" id="page-content">
+        <div className="padding">
+          <div className="row container d-flex justify-content-center">
+            <div className="col-md-12">
+              <div className="card px-3">
+                <div className="card-body">
+                  <h4 className="card-title">Awesome Todo list</h4>
+                  <div className="add-items d-flex">
+                    {" "}
+                    <input
+                      type="text"
+                      className="form-control todo-list-input"
+                      placeholder="What do you need to do today?"
+                    />{" "}
+                    <button className="add btn btn-primary font-weight-bold todo-list-add-btn">
+                      Add
+                    </button>{" "}
+                  </div>
+                  <div className="list-wrapper">
+                    <ul className="d-flex flex-column-reverse todo-list">
+                      <li>
+                        <div className="form-check">
+                          {" "}
+                          <label className="form-check-label">
+                            {" "}
+                            <input className="checkbox" type="checkbox" /> For
+                            what reason would it be advisable.{" "}
+                            <i className="input-helper" />
+                          </label>{" "}
+                        </div>{" "}
+                        <i className="remove mdi mdi-close-circle-outline" />
+                      </li>
+                      <li className="completed">
+                        <div className="form-check">
+                          {" "}
+                          <label className="form-check-label">
+                            {" "}
+                            <input
+                              className="checkbox"
+                              type="checkbox"
+                              defaultChecked
+                            />{" "}
+                            For what reason would it be advisable for me to
+                            think. <i className="input-helper" />
+                          </label>{" "}
+                        </div>{" "}
+                        <i className="remove mdi mdi-close-circle-outline" />
+                      </li>
+                      <li>
+                        <div className="form-check">
+                          {" "}
+                          <label className="form-check-label">
+                            {" "}
+                            <input className="checkbox" type="checkbox" /> it be
+                            advisable for me to think about business content?{" "}
+                            <i className="input-helper" />
+                          </label>{" "}
+                        </div>{" "}
+                        <i className="remove mdi mdi-close-circle-outline" />
+                      </li>
+                      <li>
+                        <div className="form-check">
+                          {" "}
+                          <label className="form-check-label">
+                            {" "}
+                            <input className="checkbox" type="checkbox" /> Print
+                            Statements all <i className="input-helper" />
+                          </label>{" "}
+                        </div>{" "}
+                        <i className="remove mdi mdi-close-circle-outline" />
+                      </li>
+                      <li className="completed">
+                        <div className="form-check">
+                          {" "}
+                          <label className="form-check-label">
+                            {" "}
+                            <input
+                              className="checkbox"
+                              type="checkbox"
+                              defaultChecked
+                            />{" "}
+                            Call Rampbo <i className="input-helper" />
+                          </label>{" "}
+                        </div>{" "}
+                        <i className="remove mdi mdi-close-circle-outline" />
+                      </li>
+                      <li>
+                        <div className="form-check">
+                          {" "}
+                          <label className="form-check-label">
+                            {" "}
+                            <input className="checkbox" type="checkbox" /> Print
+                            bills <i className="input-helper" />
+                          </label>{" "}
+                        </div>{" "}
+                        <i className="remove mdi mdi-close-circle-outline" />
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-     </div>
+    </div>
   );
 }
 
